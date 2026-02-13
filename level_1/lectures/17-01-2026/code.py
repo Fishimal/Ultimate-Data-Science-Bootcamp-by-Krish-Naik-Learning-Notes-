a5be3f07e7cae8362698e7df00981b4eba9a2e83
Rubina Das <rubinadas@Rubinas-MacBook-Air.local>
2026-02-13 21:25:33 +0530

    added my python scripts

---
+++ level_1/lectures/17-01-2026/code.py
@@ -0,0 +1,24 @@
+a = 12
+b = 12
+c = 12.0
+
+# hhyfgdghkj
+print(id(a))
+id(b)
+id(c)
+
+'''
+This is Ultimate Data Science V2.0 batch
+We are learning variables in this session
+This is Ultimate Data Science V2.0 batch
+We are learning variables in this session
+This is Ultimate Data Science V2.0 batch
+We are learning variables in this session
+This is Ultimate Data Science V2.0 batch
+We are learning variables in this session
+This is Ultimate Data Science V2.0 batch
+We are learning variables in this session
+print(a)
+'''
+
+print(a)
